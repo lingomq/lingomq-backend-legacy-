0d68cf4bbc4212ca52088add05bf9985279fcd3a
prnic <domarlaid2@mail.ru>
2023-09-23 20:23:28 +0500

    configure Authentication.Api

---
+++ src/Services/Authentication/Authentication.Api/Controllers/AuthenticationController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Authentication.Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class AuthenticationController : ControllerBase
+    {
+    }
+}
--- src/Services/Authentication/Authentication.Api/Program.cs
+++ src/Services/Authentication/Authentication.Api/Program.cs
@@ -1,4 +1,14 @@
+using Authentication.BusinessLayer.Contracts;
+using Authentication.BusinessLayer.Services;
+using Authentication.BusinessLayer.Services.Repositories;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using Npgsql;
+using System.Data;
+using System.Text;
+
 var builder = WebApplication.CreateBuilder(args);
+builder.Configuration.AddJsonFile("appsettings.json");
 
 // Add services to the container.
 
@@ -6,6 +16,29 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddTransient<IDbConnection>(
+    (sp) => new NpgsqlConnection(builder.Configuration["dev-connection"]));
+builder.Services.AddTransient<IUnitOfWork, IUnitOfWork>();
+builder.Services.AddTransient<IUserRepository, UserRepository>();
+builder.Services.AddTransient<IUserRoleRepository, UserRoleRepository>();
+builder.Services.AddTransient<IUserInfoRepository, UserInfoRepository>();
+builder.Services.AddTransient<IJwtService, JwtService>();
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(o =>
+    {
+        o.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = true,
+            ValidIssuer = builder.Configuration["JWT:Issuer"],
+            ValidateAudience = true,
+            ValidAudience = builder.Configuration["JWT:Audience"],
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = true,
+            IssuerSigningKey = new SymmetricSecurityKey(
+                Encoding.UTF8.GetBytes(builder.Configuration["JWT:Key"]!)),
+        };
+    });
 
 var app = builder.Build();
 
