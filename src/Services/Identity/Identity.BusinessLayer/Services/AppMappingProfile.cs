9fcf55a98717908399f86ba11d0804dfd667a979
prnic <domarlaid2@mail.ru>
2023-09-28 18:52:06 +0500

    add user role repository realization to identity service

---
+++ src/Services/Identity/Identity.BusinessLayer/Services/AppMappingProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using Identity.BusinessLayer.Dtos;
+using Identity.DomainLayer.Entities;
+
+namespace Identity.BusinessLayer.Services
+{
+    public class AppMappingProfile : Profile
+    {
+        public AppMappingProfile()
+        {
+            CreateMap<UserDto, User>().ReverseMap();
+        }
+    }
+}
