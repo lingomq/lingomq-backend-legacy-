8397dc1f1d1ab72fed82a6adca326715fdc9c792
prnic <domarlaid2@mail.ru>
2023-09-29 16:47:18 +0500

    update program.cs

--- src/Services/Identity/Identity.Api/Program.cs
+++ src/Services/Identity/Identity.Api/Program.cs
@@ -5,6 +5,7 @@ using Identity.BusinessLayer.MassTransit.Consumers;
 using Identity.BusinessLayer.Services;
 using Identity.BusinessLayer.Services.Repositories;
 using MassTransit;
+using Microsoft.OpenApi.Models;
 using Npgsql;
 using System.Data;
 using System.Reflection;
@@ -14,7 +15,39 @@ builder.Configuration.AddJsonFile("appsettings.json");
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        Description = @"JWT Authorization header using the Bearer scheme. \r\n\r\n 
+                      Enter 'Bearer' [space] and then your token in the text input below.
+                      \r\n\r\nExample: 'Bearer 12345abcdef'",
+        Name = "Authorization",
+        In = ParameterLocation.Header,
+        Type = SecuritySchemeType.ApiKey,
+        Scheme = "Bearer",
+        BearerFormat = "JWT",
+    });
+
+    c.AddSecurityRequirement(new OpenApiSecurityRequirement()
+      {
+        {
+          new OpenApiSecurityScheme
+          {
+            Reference = new OpenApiReference
+              {
+                Type = ReferenceType.SecurityScheme,
+                Id = "Bearer"
+              },
+              Scheme = "Jwt",
+              Name = "Bearer",
+              In = ParameterLocation.Header,
+
+            },
+             new string[] {}
+          }
+        });
+});
 
 builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 builder.Services.AddTransient<IDbConnection>(
