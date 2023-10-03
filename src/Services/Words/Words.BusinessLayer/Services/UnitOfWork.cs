510110dbc40478ba6f617d2d534dcfac3d5a3e8e
prnic <domarlaid2@mail.ru>
2023-10-03 17:39:43 +0500

    add unit of work

---
+++ src/Services/Words/Words.BusinessLayer/Services/UnitOfWork.cs
@@ -0,0 +1,27 @@
+﻿using Words.BusinessLayer.Contracts;
+
+namespace Words.BusinessLayer.Services
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        public ILanguageRepository Languages { get; }
+
+        public IUserRepository Users { get; }
+
+        public IUserWordRepository UserWords { get; }
+
+        public IUserWordTypeRepository UserWordTypes { get; }
+
+        public IWordTypeRepository WordTypes { get; }
+        public UnitOfWork(ILanguageRepository languages, IUserRepository users,
+            IUserWordRepository userWords, IUserWordTypeRepository userWordTypes,
+            IWordTypeRepository wordTypes)
+        {
+            Languages = languages;
+            Users = users;
+            UserWords = userWords;
+            UserWordTypes = userWordTypes;
+            WordTypes = wordTypes;
+        }
+    }
+}
