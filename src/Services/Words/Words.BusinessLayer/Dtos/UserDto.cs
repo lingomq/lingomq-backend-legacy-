c50c8157a45ac9e7c1edb00bf9fe89e7db7364c8
prnic <domarlaid2@mail.ru>
2023-10-03 14:48:18 +0500

    add dtos

---
+++ src/Services/Words/Words.BusinessLayer/Dtos/UserDto.cs
@@ -0,0 +1,10 @@
+﻿using Words.DomainLayer.Entities;
+
+namespace Words.BusinessLayer.Dtos
+{
+    public class UserDto : BaseEntity
+    {
+        public string? Email { get; set; }
+        public string? Phone { get; set; }
+    }
+}
