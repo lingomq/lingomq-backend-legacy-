7985d5ec566d467f1c25389ae81acccd0777ea64
prnic <domarlaid2@mail.ru>
2023-10-03 15:59:34 +0500

    add exceptions

---
+++ src/Services/Words/Words.BusinessLayer/Exceptions/ClientExceptions/UnauthorizedException.cs
@@ -0,0 +1,17 @@
+﻿using LingoMq.Responses;
+using Responses.StatusCodes;
+
+namespace Words.BusinessLayer.Exceptions.ClientExceptions
+{
+    public class UnauthorizedException<T> : ExceptionBase
+    {
+        public UnauthorizedException() : base((int)ClientErrorCodes.Unauthorized,
+            "You aren't authorize")
+        { }
+        public UnauthorizedException(string message)
+            : base((int)ClientErrorCodes.Unauthorized, message) { }
+        public UnauthorizedException(T data, string message = "You aren't authorize") :
+            base(StatusCode.UnauthorizedResult(data, message))
+        { }
+    }
+}
---
+++ src/Services/Words/Words.BusinessLayer/Exceptions/ServerException/InternalServerException.cs
@@ -0,0 +1,14 @@
+﻿using LingoMq.Responses;
+
+namespace Words.BusinessLayer.Exceptions.ServerException
+{
+    public class InternalServerException : ExceptionBase
+    {
+        public InternalServerException() : base(0,
+            "Something wrong!")
+        { }
+        public InternalServerException(string message = "Something wrong") :
+            base(StatusCode.InternalServerError(message))
+        { }
+    }
+}
