54c1f4cd4de9c7bdcc15b70525e82b5162169e4d
prnic <domarlaid2@mail.ru>
2023-10-03 15:10:57 +0500

    add userworddto

---
+++ src/Services/Words/Words.BusinessLayer/Dtos/UserWordDto.cs
@@ -0,0 +1,12 @@
+﻿using Words.DomainLayer.Entities;
+
+namespace Words.BusinessLayer.Dtos
+{
+    public class UserWordDto : BaseEntity
+    {
+        public string? Word { get; set; }
+        public string? Translated { get; set; }
+        public Guid LanguageId { get; set; }
+        public Guid UserId { get; set; }
+    }
+}
