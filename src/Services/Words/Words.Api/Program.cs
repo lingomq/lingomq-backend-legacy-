c69b40cca47bb6cc1452e8d05aaeaa001d54d2b8
prnic <domarlaid2@mail.ru>
2023-10-05 14:28:34 +0500

    update program.cs

--- src/Services/Words/Words.Api/Program.cs
+++ src/Services/Words/Words.Api/Program.cs
@@ -1,15 +1,59 @@
+using FluentMigrator.Runner;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using Npgsql;
+using System.Data;
+using System.Reflection;
+using System.Text;
+using Words.BusinessLayer.Contracts;
+using Words.BusinessLayer.Services;
+using Words.BusinessLayer.Services.Repositories;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// Data layer
+builder.Services.AddTransient<IDbConnection>((sp) => new NpgsqlConnection(builder.Configuration["ConnectionStrings:Dev"]));
+builder.Services.AddTransient<ILanguageRepository, LanguageRepository>();
+builder.Services.AddTransient<IUserRepository, UserRepository>();
+builder.Services.AddTransient<IUserWordRepository, UserWordRepository>();
+builder.Services.AddTransient<IUserWordTypeRepository, UserWordTypeRepository>();
+builder.Services.AddTransient<IWordTypeRepository, WordTypeRepository>();
+builder.Services.AddTransient<IUnitOfWork, UnitOfWork>();
+
+// Authentication (current: JWT)
+builder.Services.AddAuthentication(x =>
+{
+    x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+})
+    .AddJwtBearer(o =>
+    {
+        o.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = true,
+            ValidIssuer = builder.Configuration["JWT:Issuer"],
+            ValidateAudience = true,
+            ValidAudience = builder.Configuration["JWT:Audience"],
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = true,
+            IssuerSigningKey = new SymmetricSecurityKey(
+                Encoding.UTF8.GetBytes(builder.Configuration["JWT:Key"]!))
+        };
+    });
+
+// Migrations
+builder.Services.AddFluentMigratorCore()
+        .ConfigureRunner(cr => cr
+        .AddPostgres()
+        .WithGlobalConnectionString(builder.Configuration["ConnectionStrings:Dev"])
+        .ScanIn(Assembly.GetExecutingAssembly()).For.Migrations());
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
@@ -21,5 +65,16 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
+
+// Auto migrations
+
+using (var serviceScope = app.Services.CreateScope())
+{
+    var services = serviceScope.ServiceProvider;
+
+    var runner = services.GetRequiredService<IMigrationRunner>();
+
+    runner.MigrateUp();
+}
 
 app.Run();
