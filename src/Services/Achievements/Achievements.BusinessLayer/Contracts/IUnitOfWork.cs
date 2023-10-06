57b8df53c95cbc06e3c75737b03649984d5733cf
prnic <domarlaid2@mail.ru>
2023-10-06 16:04:32 +0500

    update contract

---
+++ src/Services/Achievements/Achievements.BusinessLayer/Contracts/IUnitOfWork.cs
@@ -0,0 +1,9 @@
+﻿namespace Achievements.BusinessLayer.Contracts
+{
+    public interface IUnitOfWork
+    {
+        IAchievementRepository Achievements { get; }
+        IUserAchievementRepository UserAchievements { get; }
+        IUserRepository Users { get; }
+    }
+}
