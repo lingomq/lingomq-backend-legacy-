e5c762ea25fbf00abba87584f40cfb790e950488
prnic <domarlaid2@mail.ru>
2023-10-06 17:25:12 +0500

    add unit of work repository

---
+++ src/Services/Achievements/Achievements.BusinessLayer/Services/UnitOfWork.cs
@@ -0,0 +1,21 @@
+﻿using Achievements.BusinessLayer.Contracts;
+
+namespace Achievements.BusinessLayer.Services
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        public IAchievementRepository Achievements { get; }
+
+        public IUserAchievementRepository UserAchievements { get; }
+
+        public IUserRepository Users { get; }
+        public UnitOfWork(IAchievementRepository achievementRepository,
+            IUserAchievementRepository userAchievementRepository,
+            IUserRepository userRepository)
+        {
+            Achievements = achievementRepository;
+            UserAchievements = userAchievementRepository;
+            Users = userRepository;
+        }
+    }
+}
