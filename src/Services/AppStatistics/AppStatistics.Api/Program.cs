b5091b601e628500bc722d81c8b8b11d7a05bc6b
prnic <domarlaid2@mail.ru>
2023-10-08 16:01:45 +0500

    add program.cs

--- src/Services/AppStatistics/AppStatistics.Api/Program.cs
+++ src/Services/AppStatistics/AppStatistics.Api/Program.cs
@@ -1,15 +1,48 @@
-var builder = WebApplication.CreateBuilder(args);
+using AppStatistics.BusinessLayer.Contracts.DataAccess;
+using AppStatistics.BusinessLayer.Contracts.Service;
+using AppStatistics.BusinessLayer.MassTransit.Consumers;
+using AppStatistics.BusinessLayer.Services;
+using AppStatistics.BusinessLayer.Services.DataAccess;
+using MassTransit;
 
-// Add services to the container.
+var builder = WebApplication.CreateBuilder(args);
+builder.Configuration.AddJsonFile("appsettings.json");
+
+// DataLayer
+builder.Services.AddTransient<IMongoDbFactory>((sp) => new MongoDbFactory(builder.Configuration["ConnectionStrings:MongoDb"]!));
+builder.Services.AddTransient<IAppStatisticsDataAccessService, AppStatisticsDataAccessService>();
+builder.Services.AddTransient<IAppStatisticsService, AppStatisticsService>();
+
+// MassTransit
+builder.Services.AddMassTransit(x =>
+{
+    x.SetKebabCaseEndpointNameFormatter();
+    x.AddDelayedMessageScheduler();
+    x.AddConsumer<AppStatisticsCreateOrUpdateConsumer>();
+    x.UsingRabbitMq((context, cfg) =>
+    {
+        cfg.Host("localhost", "/", h =>
+        {
+            h.Username(builder.Configuration["RabbitMq:UserName"]);
+            h.Password(builder.Configuration["RabbitMq:Password"]);
+        });
+
+        cfg.ReceiveEndpoint(typeof(AppStatisticsCreateOrUpdateConsumer).Name.ToLower(), endpoint =>
+        {
+            endpoint.ConfigureConsumer<AppStatisticsCreateOrUpdateConsumer>(context);
+        });
+        cfg.ClearSerialization();
+        cfg.UseRawJsonSerializer();
+        cfg.ConfigureEndpoints(context);
+    });
+});
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
