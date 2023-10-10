6672c08861971c717558b2add69a0af00f7d7159
prnic <domarlaid2@mail.ru>
2023-10-10 16:56:06 +0500

    add unit of work

---
+++ src/Services/Notifications/Notifications.BusinessLayer/Contracts/IUnitOfWork.cs
@@ -0,0 +1,9 @@
+﻿namespace Notifications.BusinessLayer.Contracts;
+
+public interface IUnitOfWork
+{
+    public INotificationRepository Notifications { get; }
+    public INotificationTypeRepository NotificationTypes { get; }
+    public IUserNotificationRepository UserNotifications { get; }
+    public IUserRepository Users { get; }
+}
---
+++ src/Services/Notifications/Notifications.BusinessLayer/Services/UnitOfWork.cs
@@ -0,0 +1,22 @@
+﻿using Notifications.BusinessLayer.Contracts;
+
+namespace Notifications.BusinessLayer.Services;
+
+public class UnitOfWork : IUnitOfWork
+{
+    public INotificationRepository Notifications { get; }
+    public INotificationTypeRepository NotificationTypes { get; }
+    public IUserNotificationRepository UserNotifications { get; }
+    public IUserRepository Users { get; }
+
+    public UnitOfWork(INotificationRepository notificationRepository,
+        INotificationTypeRepository notificationTypeRepository,
+        IUserNotificationRepository userNotificationRepository,
+        IUserRepository userRepository)
+    {
+        Notifications = notificationRepository;
+        NotificationTypes = notificationTypeRepository;
+        UserNotifications = userNotificationRepository;
+        Users = userRepository;
+    }
+}
