5dd31bbc6a68cbd3756fbcc313ccf8ed7b525461
prnic <domarlaid2@mail.ru>
2023-10-10 17:02:42 +0500

    add dtos

---
+++ src/Services/Notifications/Notifications.BusinessLayer/Dtos/NotificationDto.cs
@@ -0,0 +1,8 @@
+﻿namespace Notifications.BusinessLayer.Dtos;
+
+public class NotificationDto
+{
+    public string? Title { get; set; }
+    public string? Content { get; set; }
+    public Guid NotificationId { get; set; }
+}
---
+++ src/Services/Notifications/Notifications.BusinessLayer/Dtos/UserNotificationDto.cs
@@ -0,0 +1,9 @@
+﻿namespace Notifications.BusinessLayer.Dtos;
+
+public class UserNotificationDto
+{
+    public Guid UserId { get; set; }
+    public Guid NotificationId { get; set; }
+    public DateTime DateOfReceipt { get; set; }
+    public bool IsReaded { get; set; }
+}
