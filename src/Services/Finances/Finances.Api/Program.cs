b461f0f974b933e79194379c5c15f570754addcf
prn-ic <domarlaid2@mail.ru>
2023-10-11 20:42:37 +0500

    add program.cs

--- src/Services/Finances/Finances.Api/Program.cs
+++ src/Services/Finances/Finances.Api/Program.cs
@@ -1,15 +1,133 @@
-var builder = WebApplication.CreateBuilder(args);
+using System.Data;
+using System.Reflection;
+using System.Text;
+using Finances.BusinessLayer.Contracts;
+using Finances.BusinessLayer.MassTransit.Consumers;
+using Finances.BusinessLayer.Services;
+using Finances.BusinessLayer.Services.Repositories;
+using FluentMigrator.Runner;
+using MassTransit;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using Npgsql;
 
-// Add services to the container.
+var builder = WebApplication.CreateBuilder(args);
+builder.Configuration.AddJsonFile("appsettings.json");
+
+// Data Layer
+builder.Services.AddTransient<IDbConnection>((sp) =>
+    new NpgsqlConnection(builder.Configuration["ConnectionStrings:Dev"]));
+builder.Services.AddTransient<IFinanceRepository, FinanceRepository>();
+builder.Services.AddTransient<IUserFinanceRepository, UserFinanceRepository>();
+builder.Services.AddTransient<IUserRepository, UserRepository>();
+builder.Services.AddTransient<IUnitOfWork, UnitOfWork>();
+
+// Add other services
+builder.Services.AddTransient<IPaymentService>();
+
+// Authentication (current: JWT)
+builder.Services.AddAuthentication(x =>
+{
+    x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+})
+    .AddJwtBearer(o =>
+    {
+        o.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = true,
+            ValidIssuer = builder.Configuration["JWT:Issuer"],
+            ValidateAudience = true,
+            ValidAudience = builder.Configuration["JWT:Audience"],
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = true,
+            IssuerSigningKey = new SymmetricSecurityKey(
+                Encoding.UTF8.GetBytes(builder.Configuration["JWT:Key"]!))
+        };
+    });
+
+// Migrations
+builder.Services.AddFluentMigratorCore()
+        .ConfigureRunner(cr => cr
+        .AddPostgres()
+        .WithGlobalConnectionString(builder.Configuration["ConnectionStrings:Dev"])
+        .ScanIn(Assembly.GetExecutingAssembly()).For.Migrations());
+
+// MassTransit
+builder.Services.AddMassTransit(x =>
+{
+    x.SetKebabCaseEndpointNameFormatter();
+    x.AddDelayedMessageScheduler();
+    x.AddConsumer<IdentityDeleteUserConsumer>();
+    x.AddConsumer<IdentityUpdateUserConsumer>();
+    x.AddConsumer<IdentityCreateUserConsumer>();
+    x.UsingRabbitMq((context, cfg) =>
+    {
+        cfg.Host("localhost", "/", h =>
+        {
+            h.Username(builder.Configuration["RabbitMq:UserName"]);
+            h.Password(builder.Configuration["RabbitMq:Password"]);
+        });
+
+        cfg.ReceiveEndpoint(typeof(IdentityDeleteUserConsumer).Name.ToLower(), endpoint =>
+        {
+            endpoint.ConfigureConsumer<IdentityDeleteUserConsumer>(context);
+        });
+        cfg.ReceiveEndpoint(typeof(IdentityUpdateUserConsumer).Name.ToLower(), endpoint =>
+        {
+            endpoint.ConfigureConsumer<IdentityUpdateUserConsumer>(context);
+        });
+        cfg.ReceiveEndpoint(typeof(IdentityCreateUserConsumer).Name.ToLower(), endpoint =>
+        {
+            endpoint.ConfigureConsumer<IdentityCreateUserConsumer>(context);
+        });
+        cfg.ClearSerialization();
+        cfg.UseRawJsonSerializer();
+        cfg.ConfigureEndpoints(context);
+    });
+});
+
+// Swagger Auth interface
+builder.Services.AddSwaggerGen(c =>
+{
+    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        Description = @"JWT Authorization header using the Bearer scheme. \r\n\r\n 
+                      Enter 'Bearer' [space] and then your token in the text input below.
+                      \r\n\r\nExample: 'Bearer 12345abcdef'",
+        Name = "Authorization",
+        In = ParameterLocation.Header,
+        Type = SecuritySchemeType.ApiKey,
+        Scheme = "Bearer",
+        BearerFormat = "JWT",
+    });
+
+    c.AddSecurityRequirement(new OpenApiSecurityRequirement()
+      {
+        {
+          new OpenApiSecurityScheme
+          {
+            Reference = new OpenApiReference
+              {
+                Type = ReferenceType.SecurityScheme,
+                Id = "Bearer"
+              },
+              Scheme = "Jwt",
+              Name = "Bearer",
+              In = ParameterLocation.Header,
+
+            },
+             new string[] {}
+          }
+        });
+});
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
--- src/Services/Finances/Finances.BusinessLayer/Contracts/IPaymentService.cs
+++ src/Services/Finances/Finances.BusinessLayer/Contracts/IPaymentService.cs
@@ -1,8 +1,10 @@
-﻿namespace Finances.BusinessLayer.Contracts
+﻿using Finances.BusinessLayer.Models.YooKassa;
+
+namespace Finances.BusinessLayer.Contracts
 {
     public interface IPaymentService
     {
-        Task<bool> ConfirmPayment();
-        Task<bool> ConfirmPaymentData();
+        Task<bool> ConfirmPayment(Guid paymentId);
+        Task<bool> ConfirmPaymentData(Guid userId, Guid financeId, YooKassaSuccessResponse response);
     }
 }
