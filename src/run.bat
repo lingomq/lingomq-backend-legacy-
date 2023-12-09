43a9ec01e392cb0edf3e31c9b6865a261a501f27
zmqp <81153204+prn-ic@users.noreply.github.com>
2023-12-09 19:10:43 +0500

    Delete src/run.bat

--- src/run.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-
-dotnet dev-certs https -ep %USERPROFILE%\.aspnet\https\aspnetapp.pfx -p password && dotnet dev-certs https --trust
-docker-compose -f .\docker-compose.windows.dev.yaml --verbose up --build
