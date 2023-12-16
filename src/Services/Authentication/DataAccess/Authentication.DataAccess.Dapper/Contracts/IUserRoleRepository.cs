20336389e1d7ea988e25ff6c2430b0a6720b1496
prnic <domarlaid2@mail.ru>
2023-12-16 16:08:57 +0500

    Task #56: added domain and data access projects

---
+++ src/Services/Authentication/DataAccess/Authentication.DataAccess.Dapper/Contracts/IUserRoleRepository.cs
@@ -0,0 +1,10 @@
+﻿using Authentication.Domain.Contracts;
+using Authentication.Domain.Entities;
+
+namespace Authentication.DataAccess.Dapper.Contracts;
+
+public interface IUserRoleRepository : IGenericRepository<UserRole>
+{
+    Task<UserRole?> GetByNameAsync(string name);
+}
+
---
+++ src/Services/Authentication/DataAccess/Authentication.DataAccess.Dapper/Utils/GenericRepository.cs
@@ -0,0 +1,27 @@
+﻿using Authentication.Domain.Entities;
+using Dapper;
+using System.Data;
+using System.Transactions;
+
+namespace Authentication.DataAccess.Dapper.Contracts;
+public class GenericRepository<T> where T : EntityBase
+{
+    private readonly IDbConnection _connection;
+    protected GenericRepository(IDbConnection connection) =>
+        _connection = connection;
+    protected virtual async Task<List<T>> GetByQueryAsync<E>(string sql, E entity) where E : class
+    {
+        IEnumerable<T> values;
+        values = await _connection.QueryAsync<T>(sql, entity);
+
+        return values.Count() == 0 ? new List<T>() : values.ToList();
+    }
+    protected virtual async Task ExecuteByTemplateAsync<TE>(string sql, TE entity) where TE : class
+    {
+        using var transactionScope = new TransactionScope(TransactionScopeAsyncFlowOption.Enabled);
+
+        await _connection.ExecuteAsync(sql, entity);
+        transactionScope.Complete();
+        transactionScope.Dispose();
+    }
+}
