1f9f593f6b0e302fbc27f65166819cdb7a6fad7e
prn-ic <domarlaid2@mail.ru>
2024-01-01 20:48:14 +0500

    Task #61: Updated a API layer

---
+++ src/Services/Achievements/WebApi/Controllers/UserAchievementController.cs
@@ -0,0 +1,43 @@
+﻿using Achievements.Domain.Contracts;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Achievements.Domain.Constants;
+using Achievements.Domain.Entities;
+using System.Security.Claims;
+using LingoMq.Responses;
+
+namespace Achievements.WebApi.Controllers;
+[Route("api/achievements/user")]
+[ApiController]
+public class UserAchievementController : ControllerBase
+{
+    private readonly IUserAchievementService _userAchievementService;
+    public UserAchievementController(IUserAchievementService userAchievementService) =>
+        _userAchievementService = userAchievementService;
+    private Guid UserId => new Guid(User.Claims.Where(x => x.Type == ClaimTypes.NameIdentifier)
+        .FirstOrDefault()?.Value!);
+
+    [HttpGet]
+    [Authorize(Roles = AccessRoles.Everyone)]
+    public async Task<IActionResult> Get()
+    {
+        List<UserAchievement> achievements = await _userAchievementService.GetAsync(UserId);
+        return LingoMqResponse.OkResult(achievements);
+    }
+
+    [HttpGet("{userId}")]
+    [Authorize(Roles = AccessRoles.Everyone)]
+    public async Task<IActionResult> Get(Guid userId)
+    {
+        List<UserAchievement> achievements = await _userAchievementService.GetAsync(UserId);
+        return LingoMqResponse.OkResult(achievements);
+    }
+
+    [HttpGet("count/{userId}")]
+    [Authorize(Roles = AccessRoles.Everyone)]
+    public async Task<IActionResult> GetAchievemensCount(Guid userId)
+    {
+        int count = await _userAchievementService.GetAchievementsCount(userId);
+        return LingoMqResponse.OkResult(new { Count = count });
+    }
+}
---
+++ src/Services/Achievements/WebApi/Program.cs
@@ -0,0 +1,58 @@
+using Achievements.Application.Services.DataMigrator;
+using Achievements.WebApi.Middlewares;
+using FluentMigrator.Runner;
+using System.Reflection;
+
+namespace Achievements.WebApi;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+        builder.Configuration
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json", true, true)
+            .AddEnvironmentVariables();
+
+        builder.Services.AddControllers();
+        builder.Services.AddEndpointsApiExplorer(); 
+        builder.Services.AddSwagger();
+        builder.Services.AddPostgresDataAccess(builder.Configuration);
+        builder.Services.AddApplicationMassTransit(builder.Configuration);
+        builder.Services.AddApplicationServices();
+        builder.Services.AddJwtAuth(builder.Configuration);
+        builder.Services.AddFluentMigratorCore()
+        .ConfigureRunner(cr => cr
+        .AddPostgres()
+        .WithGlobalConnectionString(builder.Configuration["ConnectionStrings:Dev:Achievements"])
+        .ScanIn(Assembly.GetAssembly(typeof(Achievements.DataAccess.Dapper.Postgres.Migrations.InitialMigration))).For.Migrations());
+
+        var app = builder.Build();
+
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
+        }
+
+        app.UseAuthentication();
+        app.UseAuthorization();
+
+        app.MapControllers();
+        app.UseMiddleware<ExceptionCatchingMiddleware>();
+
+        using (var serviceScope = app.Services.CreateScope())
+        {
+            var services = serviceScope.ServiceProvider;
+
+            var runner = services.GetRequiredService<IMigrationRunner>();
+            var dataMigrator = services.GetRequiredService<IDataMigrator>();
+            runner.MigrateUp();
+
+            dataMigrator.MigrateAsync().Wait();
+        }
+
+        app.Run();
+    }
+}
