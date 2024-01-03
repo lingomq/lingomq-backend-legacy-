ddc44115ac30587baee5ef4d4d24c3260c69c46f
prn-ic <domarlaid2@mail.ru>
2024-01-03 19:44:19 +0500

    Task #62: added application layer

--- src/Services/Topics/Topics.BusinessLayer/Contracts/IUserRoleRepository.cs
+++
@@ -1,9 +0,0 @@
-﻿using Topics.DomainLayer.Entities;
-
-namespace Topics.BusinessLayer.Contracts
-{
-    public interface IUserRoleRepository : IGenericRepository<UserRole>
-    {
-        Task<UserRole?> GetByNameAsync(string name);
-    }
-}
