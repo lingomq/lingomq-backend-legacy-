4df04c0f14f926cdf491ec22999235b3884b0bff
prn-ic <domarlaid2@mail.ru>
2024-01-03 15:29:57 +0500

    Task #62: added data access layer

---
+++ src/Services/Topics/DataAccess/DataAccess.Dapper/Postgres/RawQueries/TopicStatisticsQueries.cs
@@ -0,0 +1,48 @@
+﻿namespace Topics.DataAccess.Dapper.Postgres.RawQueries;
+public static class TopicStatisticsQueries 
+{
+    public static readonly string Get =
+        "SELECT topic_statistics.id, " +
+        "statistics_date as \"StatisticsDate\", " +
+        "topics.id, " +
+        "topics.title as \"Title\", " +
+        "topics.content as \"Content\", " +
+        "topics.icon as \"Icon\"," +
+        "topics.creational_date as \"CreationalDate\", " +
+        "languages.id, " +
+        "languages.name as \"Name\", " +
+        "topic_levels.id, " +
+        "topic_levels.name as \"LevelName\"," +
+        "users.id, " +
+        "users.email as \"Email\", " +
+        "users.phone as \"Phone\", " +
+        "topic_statistics_types.id," +
+        "topic_statistics_types.name as \"TypeName\" " +
+        "FROM topic_statistics " +
+        "JOIN topics ON topics.id = topic_statistics.topic_id " +
+        "JOIN languages ON languages.id = topics.language_id" +
+        "JOIN topic_levels ON topic_levels.id = topics.topic_level_id " +
+        "JOIN users ON users.id = topic_statistics.user_id " +
+        "JOIN topic_statistics_types ON topic_statistics_types.id = topic_statistics.topic_statistics_type_id ";
+    public static readonly string GetRange = Get +
+        "LIMIT @Count";
+    public static readonly string GetById = Get +
+        "WHERE topic_statistics.id = @Id";
+    public static readonly string GetByTopicId = Get +
+        "WHERE topics.id = @Id";
+    public static readonly string Create =
+        "INSERT INTO topic_statistics " +
+        "(id, topic_id, user_id, topic_statistics_type_id, statistics_date) " +
+        "VALUES " +
+        "(@Id, @TopicId, @UserId, @StatisticsTypeId, @StatisticsDate)";
+    public static readonly string Update =
+        "UPDATE topic_statistics SET " +
+        "topic_id = @TopicId, " +
+        "user_id = @UserId, " +
+        "statistics_type_id = @StatisticsTypeId," +
+        "statistics_date = @StatisticsDate " +
+        "WHERE id = @Id";
+    public static readonly string Delete =
+        "DELETE FROM topic_statistics " +
+        "WHERE id = @Id";
+}
---
+++ src/Services/Topics/DataAccess/DataAccess.Dapper/Postgres/Realizations/TopicLevelRepository.cs
@@ -0,0 +1,27 @@
+﻿using System.Data;
+using Topics.DataAccess.Dapper.Contracts;
+using Topics.DataAccess.Dapper.Postgres.RawQueries;
+using Topics.DataAccess.Dapper.Utils;
+using Topics.Domain.Entities;
+
+namespace Topics.DataAccess.Dapper.Postgres.Realizations;
+public class TopicLevelRepository : GenericRepository<TopicLevel>, ITopicLevelRepository
+{
+    public TopicLevelRepository(IDbConnection connection) : base(connection)
+    {
+    }
+
+    public async Task AddAsync(TopicLevel entity) =>
+        await ExecuteByTemplateAsync(TopicLevelQueries.Create, entity);
+    public async Task DeleteAsync(Guid id) =>
+        await ExecuteByTemplateAsync(TopicLevelQueries.Delete, new { Id = id });
+    
+    public async Task<List<TopicLevel>> GetAsync(int range = int.MaxValue) =>
+        await QueryListAsync(TopicLevelQueries.GetRange, new { Count = range });
+    
+    public async Task<TopicLevel?> GetByIdAsync(Guid id) =>
+        await QueryFirstAsync(TopicLevelQueries.GetById, new { Id = id });
+
+    public async Task UpdateAsync(TopicLevel entity) =>
+        await ExecuteByTemplateAsync(TopicLevelQueries.Update, entity);
+}
