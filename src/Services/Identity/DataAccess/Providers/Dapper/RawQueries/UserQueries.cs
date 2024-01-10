de13b5eb3fb5164e2a27d3620bf9b42b7765eb4f
prn-ic <domarlaid2@mail.ru>
2024-01-10 16:21:58 +0500

    Added queries

---
+++ src/Services/Identity/DataAccess/Providers/Dapper/RawQueries/UserQueries.cs
@@ -0,0 +1,30 @@
+﻿namespace Identity.DataAccess.Providers.Dapper.RawQueries;
+public static class UserQueries
+{
+    public static readonly string SelectAll =
+        @"SELECT users.id,
+          users.nickname as ""Nickname"",
+          users.image_uri as ""ImageUri"", 
+          roles.id, 
+          roles.name as ""Name""
+          FROM users 
+          LEFT JOIN roles ON users.role_id = roles.id";
+    public static readonly string SelectById =
+        SelectAll + " WHERE id = @Id";
+    public static readonly string SelectRange =
+        SelectAll + " ORDER BY nickname DESC OFFSET (@Skip) ROWS FETCH NEXT (@Take) ROWS ONLY";
+    public static readonly string Insert =
+        "INSERT INTO users, " +
+        "(id, nickname, image_uri, role_id) " +
+        "VALUES " +
+        "(@Id, @Nickname, @ImageUri, @RoleId)";
+    public static readonly string Update =
+        "UPDATE users " +
+        "SET nickname = @Nickname, " +
+        "image_uri = @ImageUri, " +
+        "role_id = @RoleId " +
+        "WHERE id = @Id";
+    public static readonly string Delete =
+        "DELETE FROM users " +
+        "WHERE id = @Id";
+}
