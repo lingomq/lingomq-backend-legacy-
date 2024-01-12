044a4f36ad996db2701c533e32dd52d2fadde6d9
prn-ic <domarlaid2@mail.ru>
2024-01-12 20:06:18 +0500

    add event bus parameters

---
+++ src/Services/Identity/Application/EventBus/MassTransit/PublisherBase.cs
@@ -0,0 +1,21 @@
+﻿using MassTransit;
+using Microsoft.Extensions.Logging;
+
+namespace Identity.Application.EventBus.MassTransit;
+public class PublisherBase
+{
+    private IBus _bus;
+    private readonly ILogger<PublisherBase> _logger;
+    public PublisherBase(IBus bus, ILogger<PublisherBase> logger)
+    {
+        _bus = bus;
+        _logger = logger;
+    }
+    public async Task SendAsync<T>(T entity, CancellationToken cancellationToken = default) where T : class
+    {
+        var sendPoint = await _bus.GetPublishSendEndpoint<T>();
+        Type entityType = typeof(T);
+        _logger.LogInformation("[+] [Identity Publisher] Send to {0}", entityType);
+        await sendPoint.Send(entity, cancellationToken);
+    }
+}
