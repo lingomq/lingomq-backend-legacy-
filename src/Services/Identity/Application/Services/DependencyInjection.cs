c1ba3a509b3528af378ed3fa0fc7698c987cbc5b
prn-ic <domarlaid2@mail.ru>
2024-01-18 21:12:02 +0500

    added api project and add dependency injection class

---
+++ src/Services/Identity/Application/Services/DependencyInjection.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.Configuration;
+using Identity.Domain.Contracts;
+using Identity.Application.Services.Authentication;
+using Identity.Application.Services.Confirmation;
+using Identity.Application.Services.UserActions;
+using Identity.Application.Services.Jwt;
+
+namespace Microsoft.Extensions.DependencyInjection;
+public static partial class DependencyInjection
+{
+    public static IServiceCollection AddPostgresDataAccess(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddScoped<IJwtService, JwtService>();
+        services.AddScoped<IAuthenticationService, AuthenticationService>();
+        services.AddScoped<IConfirmationService, ConfirmationService>();
+        services.AddScoped<IUserService, UserService>();
+
+        return services;
+    }
+}
