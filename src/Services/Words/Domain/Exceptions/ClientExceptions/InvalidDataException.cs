2ff1b2a530240023c8e1e90623cc39b705a9d74f
prn-ic <domarlaid2@mail.ru>
2024-01-20 22:14:01 +0500

    Refactored a words service

---
+++ src/Services/Words/Domain/Exceptions/ClientExceptions/InvalidDataException.cs
@@ -0,0 +1,21 @@
+﻿using LingoMq.Responses;
+using Responses.StatusCodes;
+
+namespace Words.Domain.Exceptions.ClientExceptions;
+
+public class InvalidDataException<T> : ExceptionBase
+{
+    public InvalidDataException() : base((int)ClientErrorCodes.BadRequest, "Invalid data") { }
+    public InvalidDataException(string message)
+        : base((int)ClientErrorCodes.BadRequest, message) { }
+    public InvalidDataException(T data, string message = "Invalid data") :
+        base(LingoMqResponse.BadRequestResult(data, message))
+    { }
+    public InvalidDataException(T data, string[] parameters) :
+        base(LingoMqResponse.BadRequestResult(data, "Some parameters is incorrect: " + string.Join(";", parameters)))
+    { }
+
+    public InvalidDataException(string[] parameters) :
+        base(LingoMqResponse.BadRequestResult("Some parameters is incorrect: " + string.Join(";", parameters)))
+    { }
+}
---
+++ src/Services/Words/Domain/Exceptions/ClientExceptions/UnauthorizedException.cs
@@ -0,0 +1,16 @@
+﻿using LingoMq.Responses;
+using Responses.StatusCodes;
+
+namespace Words.Domain.Exceptions.ClientExceptions;
+
+public class UnauthorizedException<T> : ExceptionBase
+{
+    public UnauthorizedException() : base((int)ClientErrorCodes.Unauthorized,
+        "You aren't authorize")
+    { }
+    public UnauthorizedException(string message)
+        : base((int)ClientErrorCodes.Unauthorized, message) { }
+    public UnauthorizedException(T data, string message = "You aren't authorize") :
+        base(LingoMqResponse.UnauthorizedResult(data, message))
+    { }
+}
