83edc8337106f819b2d74a3c720ec35489775e90
prn-ic <domarlaid2@mail.ru>
2024-01-24 08:08:16 +0500

    added authentication domain layer to identity domain layer

---
+++ src/Services/Identity/Domain/Contracts/IAuthenticationService.cs
@@ -0,0 +1,9 @@
+﻿using Identity.Domain.Models;
+
+namespace Identity.Domain.Contracts;
+public interface IAuthenticationService
+{
+    Task<TokenModel> SignInAsync(SignModel signUpModel, CancellationToken cancellationToken);
+    Task<int> SignUpAsync(SignModel signUpModel, CancellationToken cancellationToken);
+    Task<TokenModel> RefreshTokenAsync(string token, CancellationToken cancellationToken);
+}
---
+++ src/Services/Identity/Domain/Contracts/IConfirmationService.cs
@@ -0,0 +1,7 @@
+﻿using Identity.Domain.Models;
+
+namespace Identity.Domain.Contracts;
+public interface IConfirmationService
+{
+    Task<TokenModel> ConfirmEmailAsync(string token, CancellationToken cancellationToken);
+}
